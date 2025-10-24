7c4020a0fc26c03630399cbb740a2d394baee7da
Dima_Gulak <gamerstaject@gmail.com>
2025-10-24 08:36:36 +0300

    master: add bot

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash' });
+const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash', generationConfig: {temperature: 0.9} });
 
 const USED_TOPICS_FILE = './used_topics.json';
 let usedTopics = [];
